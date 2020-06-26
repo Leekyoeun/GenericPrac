96ca1a9bb094b910296de7875c1226e2da9af73f
Leekyoeun <s2018s35@e-mirim.hs.kr>
2020-06-26 12:06:55 +0900

    20200626

--- GenericPrac/Program.cs
+++ GenericPrac/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace GenericPrac
 {
+    class Wanted<T>
+    {
+        public T value;
+        public Wanted(T value)
+        {
+            this.value = value;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> WantedString = new Wanted<string>("루피");
+            Wanted<int> wantedInt = new Wanted<int>(100);
+            Wanted<double> wanteddouble = new Wanted<double>(3.141592);
+            Console.WriteLine(WantedString.value);
+            Console.WriteLine(wantedInt.value);
+            Console.WriteLine(wanteddouble.value);
         }
     }
 }
